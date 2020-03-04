762ccc165056dd1e10e85c5224e376fde44ee61f
FTpr <felix.toepper@gmail.com>
2020-03-04 09:53:57 +0100

    Added Git-Tool

---
+++ gitcontrol.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+from functions import *
+
+def main():
+	clear()
+	print()
+	print(" ##############################")
+	print("          Git-Menue")
+	print(" ##############################")
+	
+	print()
+	print(" 0) Zurück")
+	
+	print()
+	print(" 1) Pull from Git")
+	print(" 2) Push to Git (Master)")
+	
+	
+	print()
+	
+	inputVar = input(":")
+	if inputVar == "0":
+		exit()
+	elif inputVar == "1":
+		pull()
+	elif inputVar == "2":
+		push()
+	else:
+		input("Fehlerhafte Eingabe!")
+	main()
+
+
+def pull():
+	os.system("git pull origin master")
+
+def push():
+	committext = input("Commit-Name: ")
+	
+	os.system("git add --all")
+	os.system('git commit -m "' + committext + '"')
+
+main()
--- serverfunction.py
+++ serverfunction.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import os			#für Betriebssystemfunktionen (z.B. exit)
